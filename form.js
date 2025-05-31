7361e00db4eaa054e630f608e87123dcbf3db095
Shahzeb khan <zeb84250@gmail.com>
2025-05-31 01:27:48 -0700

    first commit

---
+++ form.js
@@ -0,0 +1,35 @@
+
+const form = document.querySelector("form");
+
+function FormHandle(e){
+e.preventDefault();
+const fullname = e.target.fullname.value;
+const email = e.target.email.value;
+const password = e.target.password.value;
+const gender = e.target.gender.value;
+const courses = e.target.course;
+const selectedCourses = [];
+for(let course of courses){
+    if(course.checked){
+        selectedCourses.push(course.value);
+    }
+}
+const province = e.target.province.value;
+const dob = e.target.dob.value;
+
+const data = {
+    fullname : fullname,
+    email: email,
+    password : password,
+    gender: gender,
+    selectedCourses: selectedCourses,
+    province: province,
+    dob : dob,
+}
+
+console.log(data);
+
+}
+form.addEventListener("submit", (evt) =>{
+    FormHandle(evt);
+});
